17b9bff531463781e2600464e304d03a592f383d
Yagnesh <102507364+yagnesh1708@users.noreply.github.com>
2022-07-02 12:36:03 +0530

    Add files via upload

--- codes/1.2.py
+++ codes/1.2.py
@@ -14,9 +14,23 @@ for i in range(0,50):
     y_len=np.size(y_ind)
     y.append(y_len/prob)
 
-plt.plot(x,y)
+z=[]
+
+for i in range(0,50):
+    if (x[i]<=0):
+         z.append(0) 
+    if (x[i]>=1):
+           z.append(1)
+    if(0<x[i]<1):
+             z.append(x[i])
+
+plt.scatter(x,z,color='red',)
+plt.plot(x,y,color='green',)
 plt.grid()
 plt.xlabel('$x$')
 plt.ylabel('$F_X(x)$')
+plt.legend(['theoretical','practical'])
 plt.savefig('uni_cdf.pdf')
 plt.savefig('uni_cdf.eps')
+
+
